3906ab6a5fffaabd18ab009a01e3ea649f7332c1
Mohammeddaguagui <Mohammeddaguagui1234@gmail.com>
2023-06-15 17:53:37 -0700

    Eyeet

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,17 @@
+#include <unistd.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 1 (error).
+ */
+
+
+int main(void)
+{
+	char quo[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+
+	write(1, qui, 59);
+	return(1);
+}
