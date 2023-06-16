0e2e843fd030ddd0a9628a047086f6a523bdea32
Mohammeddaguagui <Mohammeddaguagui1234@gmail.com>
2023-06-15 18:31:59 -0700

    vbn

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,7 +3,7 @@
 /**
  * main - Entry point
  *
- * Description: print a qout using write function ssize_t write(int fd, const void *buf, size_t count);
+ * Description: print a qout using write function;
  *
  * Return: 1 (error).
 */
