05b856afe9d95707ce80499519e9a0c51772307c
Mohammeddaguagui <Mohammeddaguagui1234@gmail.com>
2023-06-15 18:09:37 -0700

    cv

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,15 +3,15 @@
 /**
  * main - Entry point
  *
- * Return: Always 1 (error).
- */
+ * description: print a qout using write function ssize_t write(int fd, const void *buf, size_t count);
+ *
+ * Return: 1 (error).
+*/
 
 
 int main(void)
-{
-	char quo[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-
-
-	write(1, qui, 59);
+{	char quo[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	
+	write(1, quo, 59);
 	return(1);
 }
